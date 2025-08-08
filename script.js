d6c0b9a2dd5bcd05e8e9c4ea61dad48198d1e61e
AnandRajSingh1 <124609851+AnandRajSingh1@users.noreply.github.com>
2025-08-08 13:03:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+/*const words = ['Apple', 'bnana','grapes', 99, true,
+    {name: 'Anand'},
+    function hello() {
+        console.log("hey Whats up")
+    }
+];
+//console.log(words);
+
+//console.log(words[6]);
+const  newWords = words;
+newWords[7] = 88;
+
+console.log(words.length)*/
